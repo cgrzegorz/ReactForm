bfe91e5b1180f7f126d8f2d097e0441c4638d7b4
grzegorz.cieslak <grzegorz.cieslak@zaven.pl>
2020-11-30 23:41:19 +0100

    back init

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b14846044c49f23437256edc569668d0'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b21ad7af-1745-4f43-8082-9a9a9a21c906'
+};
